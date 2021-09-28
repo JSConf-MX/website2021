22cd3ee78131cde15235553b1808c0cd20ba085b
Flor <florivonne.bg@gmail.com>
2021-09-28 18:27:01 -0500

    FEAT about

---
+++ components/about.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="about">
+        <div class="about__header">
+        <span class="about__title">About</span>
+        </div>
+        <div class="about__container">
+            <div class="about__paragraph">
+                Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+            </div>
+            <div class="about__photo">
+                <img class="about__decoration" src="/site/about.png" alt="">
+                <img class="about__photo-img" src="/site/community.png" alt="Mexican tech community">
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#about {
+    margin-bottom: 30px;
+}
+.about {
+  &__container {
+    background-color: $white;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 15px;
+  }
+  &__header {
+    background-color: $orange;
+    padding: 10px 15px;
+  }
+  &__title {
+    font-family: $bold;
+    font-size: 26px;
+    color: $white;
+  }
+  &__photo {
+    width: 50%;
+    position: relative;
+    &-img {
+      width: 100%;
+    }
+  }
+  &__paragraph {
+    width: calc(50% - 20px);
+    color: $purple;
+    font-size: 14px;
+    font-family: $regular;
+  }
+  &__decoration {
+    width: 100%;
+    position: absolute;
+    top: -20%;
+    left: -10%;
+    max-width: 80px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <Header />
-    <div class="grid__row">
+    <div class="grid grid__row">
       <right-menu/>
       <div class="grid__container">
         <hero/>
+        <about/>
       </div>
       <div class="grid__container-side">
 
@@ -19,13 +20,15 @@ import Header from '~/components/header.vue'
 import Footer from '~/components/footer.vue'
 import rightMenu from '~/components/right-menu.vue'
 import hero from '~/components/hero.vue'
+import about from '~/components/about.vue'
 export default {
   name: 'index',
   components: {
     Header,
     Footer,
     rightMenu,
-    hero
+    hero,
+    about
   }
 }
 </script>
