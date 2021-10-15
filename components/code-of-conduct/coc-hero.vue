04713e3fb0b6f826372936de6eddce95d99c28b8
Flor <florivonne.bg@gmail.com>
2021-10-15 18:56:59 -0500

    FEAT code of conduct's hero

---
+++ components/code-of-conduct/coc-hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="coc-hero">
+    <h2 class="coc-hero__title">Codigo de conducta</h2>
+  </div>
+</template>
+
+<style lang="scss">
+.coc-hero {
+  background-image: url('../../static/site/dog-ducks.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  min-height: 245px;
+  background-position: right;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: flex-end;
+  padding: 30px 0px;
+  &__title {
+    font-size: 20px;
+    color: $white;
+    font-family: $pixeled;
+    text-align: right;
+  }
+}
+</style>
---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header />
+    <div class="grid grid__row">
+      <right-menu/>
+      <div class="grid__container">
+        <coc-hero/>
+      </div>
+      <side-bar/>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '~/components/header.vue'
+import Footer from '~/components/footer.vue'
+import rightMenu from '~/components/right-menu.vue'
+import cocHero from '~/components/code-of-conduct/coc-hero.vue'
+import sideBar from '~/components/side-bar.vue'
+export default {
+  components: {
+    Header,
+    Footer,
+    rightMenu,
+    cocHero,
+    sideBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
